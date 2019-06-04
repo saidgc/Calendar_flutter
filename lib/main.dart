33e43a2f4e265c5db69343880b8c6f8ac8033867
modulo IV <moduloIV@moduloIV.com>
2019-06-04 12:05:59 -0500

    calendario

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,67 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  Widget fila() {
+    return Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("D"),
+          SizedBox(width: 30),
+          Text("L"),
+          SizedBox(width: 30),
+          Text("M"),
+          SizedBox(width: 30),
+          Text("M"),
+          SizedBox(width: 30),
+          Text("J"),
+          SizedBox(width: 30),
+          Text("V"),
+          SizedBox(width: 30),
+          Text("S")
+        ]);
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.white));
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calendario',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         body: Container(
-          color: Colors.white,
-        ),
+            color: Colors.white,
+            child: Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Center(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                    Center(
+                        child: Text("JUN",
+                            style: TextStyle(
+                                fontSize: 30.0, fontWeight: FontWeight.w400))),
+                    SizedBox(height: 30),
+                    Center(child: fila()),
+                    SizedBox(height: 30),
+                    Center(child: fila()),
+                    SizedBox(height: 30),
+                    Center(child: fila()),
+                    SizedBox(height: 30),
+                    Center(child: fila()),
+                    SizedBox(height: 30),
+                    Center(child: fila()),
+                    SizedBox(height: 30),
+                    Center(child: fila())
+                  ])),
+            )),
       ),
     );
   }
 }
-
